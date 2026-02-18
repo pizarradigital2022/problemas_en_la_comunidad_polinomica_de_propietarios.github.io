b7a87f80a80a991276e404ad1302fc56767ee9b8
Joaquín <pizarradigital2022@gmail.com>
2026-02-18 21:39:49 +0100

    Add files via upload

--- theme/style.js
+++ theme/style.js
@@ -43,6 +43,7 @@ var myTheme = {
                         myTheme.params('add');
                     }
                 }
+                window.scroll(0, 0);
             } else {
                 $('body').toggleClass('siteNav-off');
                 myTheme.params(
@@ -61,6 +62,7 @@ var myTheme = {
                 }
                 bar.show();
                 $('#exe-client-search-text').focus();
+                window.scroll(0, 0);
             }
         });
         if (!this.inIframe()) {
@@ -73,9 +75,6 @@ var myTheme = {
         }
         // Search form
         this.searchForm();
-
-        // mover .page-title dentro de .page-content
-        this.movePageTitle();
     },
     inIframe: function () {
         try {
@@ -119,43 +118,10 @@ var myTheme = {
             myTheme.param(this, act);
         });
     },
-
-    // function that move the h2 outside the header
-    movePageTitle: function () {
-        const tryMove = () => {
-            const $header = $('.main-header .page-header');
-            const $title = $header.find('.page-title').first();
-
-            // Search container of content
-            let $content = $('.page-content').first();
-            if (!$content.length)
-                $content = $('.content, main .content').first();
-            if (!$content.length) $content = $('#main, #content').first();
-            if (!$content.length && $header.length)
-                $content = $header.nextAll(':not(header)').first();
-            if (!$content.length && $header.length) $content = $header.parent();
-
-            if ($header.length && $title.length && $content.length) {
-                $content.prepend($title); // move it to the start
-                return true;
-            }
-            return false;
-        };
-
-        if (tryMove()) return;
-
-        const observer = new MutationObserver(() => {
-            if (tryMove()) observer.disconnect();
-        });
-        observer.observe(document.body, { childList: true, subtree: true });
-    },
-    // 🔼
 };
-
 $(function () {
     myTheme.init();
 });
-
 $.fn.isInViewport = function () {
     var elementTop = $(this).offset().top;
     var elementBottom = elementTop + $(this).outerHeight();
